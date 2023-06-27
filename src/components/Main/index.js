7702656026386e535bdaff571f4bfeeff3ba621d
DmitrySimonofff <135084791+DmitrySimonofff@users.noreply.github.com>
2023-06-27 16:25:41 +0300

    fix4

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -4,7 +4,7 @@ import Choice from "../Choice";
 const Main = () => {
   return (
     <div className="d-flex wrap  flex-column justify-content-center align-items-center">
-      <h2 className="my-4">Choose Any From Below</h2>
+      <h2 className="main-hd">Make a choice</h2>
       <Choice />
     </div>
   );
