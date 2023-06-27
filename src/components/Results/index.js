19409d857988e8ebb9a48cb62eac287c7a8c1dfe
DmitrySimonofff <135084791+DmitrySimonofff@users.noreply.github.com>
2023-06-27 18:13:29 +0300

    fix9

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -34,25 +34,25 @@ const Results = () => {
     setResults(res.data.data.resultsDeclareds);
   };
 
-  console.log(results);
+//  console.log(results);
 
   return (
     <div className="d-flex justify-content-center align-items-center mw-80">
-      <table class="table table-warning mw-80 ">
+      <table class="table table-warning mw-80 tb-main">
         <thead>
           <tr>
-            <th scope="col">Poisition</th>
-            <th scope="col">Address</th>
-            <th scope="col">Bet</th>
-            <th scope="col">Result</th>
-            <th scope="col">Time</th>
+            <th scope="col" class="th-1">Poisition</th>
+            <th scope="col" class="th-2">Address</th>
+            <th scope="col" class="th-3">Bet</th>
+            <th scope="col" class="th-3">Result</th>
+            <th scope="col" class="th-4">Time</th>
           </tr>
         </thead>
         <tbody>
           {results.map((result, i) => {
             return (
               <tr key={i}>
-                <th scope="row">{i + 1}</th>
+                <th scope="row"><span>1{i + 1}</span></th>
                 <td>{shortenAddress(result._player)}</td>
                 <td>{ethers.utils.formatEther(result._amount)}</td>
                 <td>
